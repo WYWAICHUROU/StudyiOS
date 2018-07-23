fb34cda23717adc716cf96771514d418fbd635bb
闻飞 <hongfa1003@taolesoft.com>
2018-07-23 16:28:08 +0800

    Initial Commit

---
+++ newXuanZHuan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newXuanZHuan
+//
+//  Created by 闻飞 on 2018/7/23.
+//  Copyright © 2018年 WrenchTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
