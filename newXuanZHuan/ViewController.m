da5b89411a0b6f3406e9917b9ae2604a25c38059
闻飞 <hongfa1003@taolesoft.com>
2018-07-23 16:44:52 +0800

    first commit

--- newXuanZHuan/ViewController.m
+++ newXuanZHuan/ViewController.m
@@ -9,15 +9,57 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic,strong)UIImageView * showImageView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.showImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0,0,350,350)];
+    
+    [self.view addSubview:self.showImageView];
+    
+    self.showImageView.layer.cornerRadius = 175;
+    
+    self.showImageView.layer.masksToBounds = YES;
+    
+    self.showImageView.image =[UIImage imageNamed:@"1.jpg"];
+    
+    CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    
+    [rotation setToValue:@(M_PI*2)];
+    
+    [rotation setDuration:5];
+    
+    [rotation setRepeatCount:MAXFLOAT];
+    
+    [self.showImageView.layer addAnimation:rotation forKey:@"rotation"];
+    
+    
+    UIButton * showOnebutton = [[UIButton alloc]initWithFrame:CGRectMake(120, 520,40,40)];
+    
+    [self.view addSubview:showOnebutton];
+    
+    [showOnebutton addTarget:self action:@selector(ggg) forControlEvents:UIControlEventAllEvents];
+    
+    showOnebutton.backgroundColor = [UIColor redColor];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+-(void)ggg
+{
+    
+    
+    self.showImageView.layer.speed = 0;
+    
+    
+}
+
+
 
 
 - (void)didReceiveMemoryWarning {
